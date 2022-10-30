306c0c17570d1e95cff65ea1cc0fe19c66e455c3
shuvrodas13 <shuvro6589@gmail.com>
2022-10-31 03:15:22 +0600

    Variables

--- A.py
+++ A.py
@@ -1,3 +1,4 @@
+#variables
 box = 'balloons'
 print(box)
 box = 'Crackers'
@@ -7,3 +8,18 @@ print(box)
 tupperware = 'slice of pizza'
 bag = 'clothes'
 bottle = 'water'
+#common Style
+styleCamelCase = "modernPython"
+StylePascalCase = "ModernPython"
+style_snake_case = "Modern_Python"
+STYLEUPPERCASE = "I don't  wanna change"
+_super_secret = "My Pasword"
+lucyNumber4 = " Looks it has a number!"
+lucy_number_3 = " I too got a number"
+print(styleCamelCase)
+print(StylePascalCase)
+print(style_snake_case)
+print(STYLEUPPERCASE)
+print(_super_secret)
+print(lucyNumber4)
+print(lucy_number_3)
